b378722459c0752eb3be93e4cb22bcfa2277a779
Boyana Giridhar <boyana.giridhar@hcl.com>
2022-07-29 01:10:29 +0530

    DB

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ import express from 'express'
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
-import notFoundMiddleware from './middleware/not-found.js' //middleware
-import errorMiddleware from './middleware/error-handler.js' //middleware
+
+import connectDB from './db/connect.js'
+
+//middleware
+import notFoundMiddleware from './middleware/not-found.js' 
+import errorMiddleware from './middleware/error-handler.js'
 
 app.get('/', (req,res) => {
     res.send("Welcome")
@@ -13,4 +17,15 @@ app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log('server is listening on port ${port}...'))
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log('server is listening on port ${port}...')
+        })
+    } catch (error) {
+        console.log('error')
+    }
+}
+
+start()
