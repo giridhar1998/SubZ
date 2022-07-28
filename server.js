e8cf4b3849f56cbd9367b2ac2023961dfe46891e
Boyana Giridhar <boyana.giridhar@hcl.com>
2022-07-29 00:55:09 +0530

    server_2

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express'
 const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
 import notFoundMiddleware from './middleware/not-found.js' //middleware
+import errorMiddleware from './middleware/error-handler.js' //middleware
 
 app.get('/', (req,res) => {
     res.send("Welcome")
