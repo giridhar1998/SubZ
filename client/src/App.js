9d87fcac5fc6e4fe30ceded520e7392349cfe964
Boyana Giridhar <boyana.giridhar@hcl.com>
2022-09-14 19:10:49 +0530

    V_1_1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Register, Landing, Error, ProtectedRoute } from './pages'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<div>Dashboard</div>} />
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
@@ -12,5 +13,11 @@ function App() {
     </BrowserRouter>
   )
 }
+
+<nav>
+  <Link to='/'>Dashboard</Link>
+  <Link to='/register'>Register</Link>
+  <Link to='/landing'>Home</Link>
+</nav>
 
 export default App;
