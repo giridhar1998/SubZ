20cd2fb4d747f859d3404dd332bb072523cd1977
Boyana Giridhar <boyana.giridhar@hcl.com>
2024-01-27 08:29:01 +0530

    package update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { combineReducers, applyMiddleware } from 'redux'
+import { legacy_createStore as createStore} from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import {
